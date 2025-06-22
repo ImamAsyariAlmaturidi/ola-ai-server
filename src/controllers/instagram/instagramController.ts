27cf1c67bc03239e84f26b7082b77a8d9acc1a50
Imam Asyari <imamasyari700@gmail.com>
2025-06-22 22:57:58 +0700

    feat: instagram oauth

--- src/controllers/instagram/instagramController.ts
+++ src/controllers/instagram/instagramController.ts
@@ -36,10 +36,10 @@ export class InstagramController {
       const url =
         `https://www.instagram.com/oauth/authorize?` +
         `client_id=${INSTAGRAM_CLIENT_ID}` +
-        `&redirect_uri=${INSTAGRAM_REDIRECT_URI}` +
-        `&scope=${scope}` +
+        `&redirect_uri=${encodeURIComponent(INSTAGRAM_REDIRECT_URI)}` +
+        `&scope=${encodeURIComponent(scope)}` +
         `&response_type=code` +
-        `&state=${state}` +
+        `&state=${encodeURIComponent(state)}` +
         `&enable_fb_login=0&force_authentication=1`;
 
       return res.redirect(url);
